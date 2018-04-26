d5d7f0a0ed5448cb5d9fa664232dab94af05bab7
winderbinder <junior_19000@hotmail.com>
2018-04-26 13:57:15 -0600

    similar to an if else statement but in a hash

---
+++ weapons_for_characters.rb
@@ -0,0 +1,14 @@
+  MAP = Hash.new("Not a character").merge(
+  "Laval" => "Laval-Shado Valious",
+  "Cragger" => "Cragger-Vengdualize",
+  "Lagravis" => "Lagravis-Blazeprowlor",
+  "Crominus" => "Crominus-Grandorius",
+  "Tormak" => "Tormak-Tygafyre",
+  "LiElla" => "LiElla-Roarburn"
+).freeze
+
+def weapons_for_people weapons
+  MAP[weapons]
+end
+
+p weapons_for_people("Laval")
