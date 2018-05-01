dcb2cfa2688ff73401469cc10d3dc4541934be1e
winderbinder <junior_19000@hotmail.com>
2018-05-01 16:35:30 -0600

    updated the method to show to examples of how to reverse numbers I added a one liner solution

--- reversing_numbers.rb
+++ reversing_numbers.rb
@@ -5,6 +5,10 @@ def digitize(n)
   stored_reversed_numbers.map { |x| x.to_i }
 end
 
-p digitize(34534)
+# here is my solution explaining the logic of reversing a string without using the .reverse method
 
-# this is another smart way of reversing numbers and I didn't use .reverse ;)
+def digitize(n)
+  split_array_numbers = n.to_s.split(//).map { |x| x.to_i}.reverse
+end
+
+# and here is a ruby one liner usig the .reverse method ;)
